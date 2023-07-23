21f67fd66b8cabf1dbb298030464292e0c5e16f6
nervouswizard <book4208@gmail.com>
2023-07-23 13:33:20 +0800

    backend manipulate

--- Pmagic.py
+++ Pmagic.py
@@ -53,7 +53,102 @@ from collections import deque
 from pyHM import mouse
 from processScript import processScript
 import configparser
+import string
+from ctypes import windll
 
+PostMessageW = windll.user32.PostMessageW
+MapVirtualKeyW = windll.user32.MapVirtualKeyW
+VkKeyScanA = windll.user32.VkKeyScanA
+
+WM_KEYDOWN = 0x100
+WM_KEYUP = 0x101
+
+VkCode = {
+    "back":  0x08,
+    "tab":  0x09,
+    "return":  0x0D,
+    "shift":  0x10,
+    "control":  0x11,
+    "menu":  0x12,
+    "pause":  0x13,
+    "capital":  0x14,
+    "escape":  0x1B,
+    "esc":  0x1B,
+    "space":  0x20,
+    "end":  0x23,
+    "home":  0x24,
+    "left":  0x25,
+    "up":  0x26,
+    "right":  0x27,
+    "down":  0x28,
+    "print":  0x2A,
+    "snapshot":  0x2C,
+    "insert":  0x2D,
+    "delete":  0x2E,
+    "lwin":  0x5B,
+    "rwin":  0x5C,
+    "numpad0":  0x60,
+    "numpad1":  0x61,
+    "numpad2":  0x62,
+    "numpad3":  0x63,
+    "numpad4":  0x64,
+    "numpad5":  0x65,
+    "numpad6":  0x66,
+    "numpad7":  0x67,
+    "numpad8":  0x68,
+    "numpad9":  0x69,
+    "multiply":  0x6A,
+    "add":  0x6B,
+    "separator":  0x6C,
+    "subtract":  0x6D,
+    "decimal":  0x6E,
+    "divide":  0x6F,
+    "f1":  0x70,
+    "f2":  0x71,
+    "f3":  0x72,
+    "f4":  0x73,
+    "f5":  0x74,
+    "f6":  0x75,
+    "f7":  0x76,
+    "f8":  0x77,
+    "f9":  0x78,
+    "f10":  0x79,
+    "f11":  0x7A,
+    "f12":  0x7B,
+    "numlock":  0x90,
+    "scroll":  0x91,
+    "lshift":  0xA0,
+    "rshift":  0xA1,
+    "lcontrol":  0xA2,
+    "rcontrol":  0xA3,
+    "lmenu":  0xA4,
+    "rmenu":  0XA5,
+    "page_up": 0x21,
+    "page_down": 0x22,
+    "caps_lock": 0x14,
+    "shift_l": 0xA0,
+    "shift_r": 0xA1,
+    "ctrl_l": 0XA2,
+    "ctrl_r": 0xA3,
+    "alt_l": 0xA4,
+    "alt_r": 0xA5,
+}
+
+def get_virtual_keycode(key: str):
+    """根据按键名获取虚拟按键码
+
+    Args:
+        key (str): 按键名
+
+    Returns:
+        int: 虚拟按键码
+    """
+    if len(key) == 1 and key in string.printable:
+        # https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-vkkeyscana
+        return VkKeyScanA(ord(key)) & 0xff
+    else:
+        return VkCode[key]
+    
 pg.KEYBOARD_MAPPING['page_up'] = 0xC9 + 1024
 pg.KEYBOARD_MAPPING['page_down'] = 0xD1 + 1024
 pg.KEYBOARD_MAPPING['caps_lock'] = 0x3A
@@ -101,6 +196,35 @@ def process_line(line):
         pg.keyDown(line[1])
     elif line[2] == 'released':
         pg.keyUp(line[1])
+
+def bg_process_line(line):
+    if line[2] == 'pressed':
+        """按下指定按键
+
+        Args:
+            handle (HWND): 窗口句柄
+            key (str): 按键名
+        """
+        vk_code = get_virtual_keycode(line[1])
+        scan_code = MapVirtualKeyW(vk_code, 0)
+        # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-keydown
+        wparam = vk_code
+        lparam = (scan_code << 16) | 1
+        PostMessageW(handle, WM_KEYDOWN, wparam, lparam)
+        
+    elif line[2] == 'released':
+        """放开指定按键
+
+        Args:
+            handle (HWND): 窗口句柄
+            key (str): 按键名
+        """
+        vk_code = get_virtual_keycode(line[1])
+        scan_code = MapVirtualKeyW(vk_code, 0)
+        # https://docs.microsoft.com/en-us/windows/win32/inputdev/wm-keyup
+        wparam = vk_code
+        lparam = (scan_code << 16) | 0XC0000001
+        PostMessageW(handle, WM_KEYUP, wparam, lparam)
 
 def read_script(filename):
     f = open(filename, mode='r')
@@ -179,8 +303,9 @@ configreader = configparser.ConfigParser()
 configreader.read('config.ini', encoding='utf-8')
 config = dict(configreader.items('Pmagic'))
 del configreader
-UsewindowTiele = config['focus_on_window_title']=='true'
 windowTiele = config['window_tiele']
+Use_background_running = config['use_background_running']=='true'
+UsewindowTiele = config['focus_on_window']=='true'
 Use_mouse_random_move = config['use_mouse_random_move']=='true'
 
 logpath = 'C:/ProgramData/Pmagic_log/Log/'
@@ -255,6 +380,11 @@ while True:
         print('執行腳本', filelist[d2])
         print = Logger.timestamped_print
         log_history(os.path.basename(__file__))
+
+        if Use_background_running:
+            handle = windll.user32.FindWindowW(None, windowTiele)
+            process_line = bg_process_line
+
         with keyboard.Listener(on_release=pause_and_continue):
             for i in range(d3):
                 if doByRows(script_path+filelist[d2], i):
