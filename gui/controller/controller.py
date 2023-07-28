d4de7e0975b5e348a16f727c37828d7ee4a1abfd
nervouswizard <book4208@gmail.com>
2023-07-28 15:24:23 +0800

    build config class

--- gui/controller/controller.py
+++ gui/controller/controller.py
@@ -1,21 +1,45 @@
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui
 from gui.GUI import Ui_MainWindow
-from setting.config import config
+from setting.config import config_reader
 import os
 
 class MainWindow_controller(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.config = config_reader('Pmagic')
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.init()
+        self.context_menu = QtWidgets.QMenu(self)
+        action1 = self.context_menu.addAction("Action 1")
+        action2 = self.context_menu.addAction("Action 2")
+        action3 = self.context_menu.addAction("Action 3")
+        self.ui.scripts_list.customContextMenuRequested.connect(self.show_context_menu)
         self.ui.search_bar_for_script.textChanged.connect(self.filter_scripts_list)
-        self.show_scripts_list()
+        self.ui.scripts_list.itemClicked.connect(self.show_selected_item)
+    # 滑鼠右鍵選單
+    def contextMenuEvent(self, event):
+        print(self.ui.scripts_list.size())
+        # Show the context menu
+        self.context_menu.exec(event.globalPos())
 
-    # 顯示所有的腳本
-    def show_scripts_list(self):
-        script_list = os.listdir(config["script_path"])
+    def init(self):
+        # 顯示所有的腳本
+        script_list = os.listdir(self.config.get("script_path"))
+        self.ui.scripts_list.clear()
         self.ui.scripts_list.addItems(script_list)
     
+    # 按滑鼠右鍵顯示
+    def show_context_menu(self, pos):
+
+        print('right click')
+        # menu = QtWidgets.QMenu(self)
+        # delete_action = QtGui.QAction('Delete', self)
+        # delete_action.triggered.connect(self.delete_script)
+        # menu.addAction(delete_action)
+        # menu.exec(self.ui.scripts_list.mapToGlobal(pos))
+
     # 搜尋腳本
     def filter_scripts_list(self, text):
         for index in range(self.ui.scripts_list.count()):
@@ -24,3 +48,11 @@ class MainWindow_controller(QtWidgets.QMainWindow):
                 item.setHidden(False)
             else:
                 item.setHidden(True)
+
+    # 顯示被選擇的腳本
+    def show_selected_item(self, item):
+        self.ui.selected_script.setText(item.text())
+
+
+    def delete_script(self):
+        print('delete')
--- setting/config.py
+++ setting/config.py
@@ -1,7 +1,28 @@
+from commentedconfigparser import CommentedConfigParser
 import configparser, os
 
-configreader = configparser.ConfigParser()
-config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
-configreader.read(config_file_path, encoding='utf-8')
-config = dict(configreader.items('Pmagic'))
-del config_file_path, configreader
+class config_reader():
+    def __init__(self, entity_title):
+        self.config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
+        self.entity_title = entity_title
+        self.config = CommentedConfigParser()
+        self.reload()
+    
+    def reload(self):
+        self.config.read(self.config_file_path, encoding='utf-8')
+        # self.config.read(self.config_file_path, encoding='utf-8')
+    
+    def set(self, entity, new_setting):
+        self.config[self.entity_title][entity] = new_setting
+        with open(self.config_file_path, mode="w", encoding='utf-8') as savefile:
+            self.config.write(savefile)
+    
+    def get(self, entity):
+        return self.config[self.entity_title][entity]
+
+if __name__ == '__main__':
+    c = config_reader('Pmagic')
+    print(c.get('window_tiele'))
+    c.set('window_tiele', 'MapleStory')
+    c.reload()
+    print(c.get('window_tiele'))
